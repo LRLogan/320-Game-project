f8eb51e28c363d737fbacce985f9e410b15969ca
LRLogan <lrl2777@g.rit.edu>
2025-09-24 13:44:52 -0400

    Progress on player movement
    Not fully functional yet

--- 320UnityProject/Assets/Scripts/Player.cs
+++ 320UnityProject/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class Player : MonoBehaviour
 {
@@ -16,25 +18,78 @@ public class Player : MonoBehaviour
     [SerializeField] private float stage3Multiplier = 2f;
     [SerializeField] private float holdToStartCharge = 1f;
     [SerializeField] private float fullChargeTime = 3f;
+    [SerializeField] private float gravity = -9.8f;
     private float jumpHoldTime = 0f;
     private bool isCharging = false;
-    private bool jumpButtonHeld = false;
+    private bool jumpButtonPressed = false;
     private bool isGrounded;
 
     // Player object
     private Rigidbody rb;
+    private CharacterController cController;
+    private Vector3 moveInput;
+    private Vector3 velocity;
 
+    // Temp use to display different stages of jump
+    private Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.FreezeRotation;
+        cController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Handeling jump input
+        if(jumpButtonPressed)
+        {
+            jumpHoldTime += Time.deltaTime;
+
+            if(jumpHoldTime >= holdToStartCharge) isCharging=true;
+
+            // Temp display to show different jump power stages
+            if(isCharging)
+            {
+                float chargeProgress = Mathf.Clamp((jumpHoldTime - holdToStartCharge) / fullChargeTime, 0f, 1f);
+                if (chargeProgress < 0.33f)
+                    rend.material.color = Color.yellow;
+                else if (chargeProgress < 0.66f)
+                    rend.material.color = new Color(1f, 0.5f, 0f);
+                else
+                    rend.material.color = Color.red;
+            }
+        }
+
+        // Jump release
+        if(true)
+        {
+
+        }
+
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        cController.Move(move * walkSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        cController.Move(velocity * Time.deltaTime);
+    }
+
+    // Listener for jump button input
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.performed && cController.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+            jumpButtonPressed = true;
+        }
+    }
+
+    // Listener for movement input
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move input: " + moveInput);
     }
 }
