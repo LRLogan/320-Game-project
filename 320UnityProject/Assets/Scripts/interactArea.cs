9539f65dde51c866fc348b72e340887c4e42f9e5
r1icard <145591997+r1icard@users.noreply.github.com>
2025-09-28 18:33:33 -0400

    new scene

--- 320UnityProject/Assets/Scripts/interactArea.cs
+++ 320UnityProject/Assets/Scripts/interactArea.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class interactArea : MonoBehaviour
 {
-    public IsoPlayerMovement playerScript;
+    public Player playerScript;
     // Start is called before the first frame update
     void Start()
     {
