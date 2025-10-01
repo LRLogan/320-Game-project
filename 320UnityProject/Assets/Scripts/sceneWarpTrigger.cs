f2408d2002947a7e6ae3cd46b4ad7fc4fe027f02
BLiOtZ-Main <Jsm1032@rit.edu>
2025-09-30 21:45:09 -0400

    warp progress

---
+++ 320UnityProject/Assets/Scripts/sceneWarpTrigger.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneWarpTrigger : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    [Tooltip("Name of the scene to load when player interacts")]
+    public string sceneToLoad;
+
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        // Check if player is in the trigger zone and presses E
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("Scene name is not set on SceneWarpTrigger.");
+        }
+    }
+
+    // Detect when player enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Debug.Log("Press E to enter the house.");
+        }
+    }
+
+    // Detect when player exits the trigger
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
