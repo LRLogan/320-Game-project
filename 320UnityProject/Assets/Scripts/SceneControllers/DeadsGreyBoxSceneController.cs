81dcdd215f8df3ad44273c5f436a368b657fc50d
LRLogan <lrl2777@g.rit.edu>
2025-10-10 13:15:56 -0400

    Code to assign inspector values dynamically in Deads greybox

--- 320UnityProject/Assets/Scripts/SceneControllers/DeadsGreyBoxSceneController.cs
+++ 320UnityProject/Assets/Scripts/SceneControllers/DeadsGreyBoxSceneController.cs
@@ -1,27 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 /// <summary>
-/// Liko other scene controllers this one aims to add game objects to the hierarchy / functionality that must be added on scene load
+/// Scene controller that attaches scene-specific UI elements to the persistent canvas when the scene loads.
 /// </summary>
 public class DeadsGreyBoxSceneController : MonoBehaviour
 {
     [SerializeField] private GameObject dialogueUIPrefab;
+    [SerializeField] private GameObject infoPannelPrefab;
+    [SerializeField] private EventSystem eventSystem;
     private Canvas canvas;
-    // Start is called before the first frame update
-    void Start()
-    {
-        canvas = FindFirstObjectByType<Canvas>();
-        Instantiate(dialogueUIPrefab);
-        dialogueUIPrefab.transform.parent = canvas.transform;
-        dialogueUIPrefab.layer = 5;
-        dialogueUIPrefab.SetActive(true);
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // Find the persistent canvas
+        canvas = GameObject.Find("Canvas")?.GetComponent<Canvas>();
+
+        if (canvas == null)
+        {
+            Debug.LogWarning("No PersistentCanvas found! Make sure one exists before loading this scene.");
+            return;
+        }
+
+        GameObject dialogueUIInstance = Instantiate(dialogueUIPrefab, canvas.transform);
+        dialogueUIInstance.layer = LayerMask.NameToLayer("UI");
+        dialogueUIInstance.SetActive(true);
+
+        GameObject infoPannelInstance = Instantiate(infoPannelPrefab, canvas.transform);
+        dialogueUIInstance.layer = LayerMask.NameToLayer("UI");
+        dialogueUIInstance.SetActive(true);
+
+        DialogueDisplay dpDisplay = eventSystem.GetComponent<DialogueDisplay>();
+        dpDisplay.onStart = true;
+        dpDisplay.lockMovement = true;
+
+        // Getting the different text components in the dialogue pannel ans assinging them 
+        TextMeshProUGUI[] textsInChild= dialogueUIInstance.GetComponentsInChildren<TextMeshProUGUI>();
+        UIController uiController = eventSystem.GetComponent<UIController>();
+        dpDisplay.dialogueBox = textsInChild[0];
+        dpDisplay.speakerBox = textsInChild[1];
     }
 }
