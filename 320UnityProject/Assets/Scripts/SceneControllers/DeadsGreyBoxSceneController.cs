1c333642dda5f53f818f29004df16a8c38afeaa8
LRLogan <lrl2777@g.rit.edu>
2025-10-14 20:28:39 -0400

    Fixed part of the bug where info panel was active

--- 320UnityProject/Assets/Scripts/SceneControllers/DeadsGreyBoxSceneController.cs
+++ 320UnityProject/Assets/Scripts/SceneControllers/DeadsGreyBoxSceneController.cs
@@ -45,5 +45,6 @@ public class DeadsGreyBoxSceneController : MonoBehaviour
 
         UIController uiController = eventSystem.GetComponent<UIController>();
         uiController.infoBox = infoPannelInstance.GetComponentInChildren<TextMeshProUGUI>();
+        infoPannelInstance.SetActive(false);
     }
 }
