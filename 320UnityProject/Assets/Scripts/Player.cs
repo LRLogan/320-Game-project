12d159a9529b56a43b1b48972d89fdc0e8d9d655
LRLogan <lrl2777@g.rit.edu>
2025-10-28 09:49:31 -0400

    More setup and play run

--- 320UnityProject/Assets/Scripts/Player.cs
+++ 320UnityProject/Assets/Scripts/Player.cs
@@ -12,8 +12,9 @@ public class Player : MonoBehaviour
     // Movement 
     [Header("Movement")]
     [SerializeField] private float walkSpeed = 5f;
-    [SerializeField] private float runSpeed = 12f;
+    [SerializeField] private float runSpeed = 10f;
     private float moveAngleOffset = 45f;
+    private bool isRunning = false;
 
     [Header("Jumping")]
     [SerializeField] private float jumpForce = 7f;
@@ -94,7 +95,15 @@ public class Player : MonoBehaviour
             return;
 
         // Player movement
-        Vector3 move = moveInput.normalized * walkSpeed;
+        Vector3 move;
+        if (isRunning)
+        {
+            move = moveInput.normalized * runSpeed;
+        }
+        else
+        {
+            move = moveInput.normalized * walkSpeed;
+        }
         Vector3 newVelocity = new Vector3(move.x, rb.velocity.y, move.z);
         rb.velocity = newVelocity;
 
@@ -142,7 +151,17 @@ public class Player : MonoBehaviour
 
     }
 
-
+    public void OnSetRun(InputAction.CallbackContext context)
+    {
+        if(context.started || context.performed)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
