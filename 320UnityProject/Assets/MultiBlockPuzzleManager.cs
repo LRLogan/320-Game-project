8907509c92ff863a007cfedf32bb9b99f096705a
r1icard <145591997+r1icard@users.noreply.github.com>
2025-11-05 11:38:18 -0500

    created puzzle wrkshop area and added multi block puzzle

--- 320UnityProject/Assets/MultiBlockPuzzleManager.cs
+++ 320UnityProject/Assets/MultiBlockPuzzleManager.cs
@@ -7,6 +7,10 @@ public class MultiBlockPuzzleManager : MonoBehaviour
     public int numberInteracted = 0;
     [SerializeField] int[] correctNumbers = new int[3];
     public int[] orderSelect = new int[3];
+    [SerializeField] bool dialogue;
+    [SerializeField] string dialogueString;
+    [SerializeField] bool item;
+    [SerializeField] GameObject objectGiven;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +31,11 @@ public class MultiBlockPuzzleManager : MonoBehaviour
             if (correctNumbers[0] == orderSelect[0] && correctNumbers[1] == orderSelect[1] && correctNumbers[2] == orderSelect[2])
             {
                 Debug.Log("yay");
+                Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+                if (player != null)
+                {
+                    player.AddToInventory(objectGiven);
+                }
             }
             else
             {
